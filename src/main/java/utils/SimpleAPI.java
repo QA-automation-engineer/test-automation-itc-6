7cc21d5a85bc6b16204b2fe9f27f658054e0f38e
Vladimir Trandafilov <trandafilov.vladimir@gmail.com>
2020-03-06 11:48:30 +0200

    feat(lesson12): captureScreenshot and other useful methods in SimpleApi
    (cherry picked from commit 80b6e2fa8fc0e4d52d9a7dad16a051bfe4072766)

--- src/main/java/utils/SimpleAPI.java
+++ src/main/java/utils/SimpleAPI.java
@@ -1,10 +1,14 @@
 package utils;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 import static utils.Conditions.VISIBLE;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.function.Function;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.*;
@@ -20,6 +24,23 @@ public abstract class SimpleAPI {
 
     protected void open(String url) {
         getDriver().get(url);
+    }
+
+    public String getPageTitle() {
+        return getDriver().getTitle();
+    }
+
+    protected void captureScreenshot(String methodName) {
+        File screenshot = ((TakesScreenshot)getDriver())
+                .getScreenshotAs(OutputType.FILE);
+        String screenshotName = screenshot.getName().replace("screenshot", methodName + "_");
+        String path = System.getProperty("report.path") + "/screenshots/" + screenshotName;
+        try {
+            FileUtils.copyFile(screenshot, new File(path));
+            LOG.error("Screenshot was got: " + screenshotName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     protected WebElement $(By locator) {
@@ -52,6 +73,26 @@ public abstract class SimpleAPI {
 
     protected List<WebElement> $$(By locator, int expNumberToBe) {
         return $$(locator, loc -> ExpectedConditions.numberOfElementsToBe(loc, expNumberToBe));
+    }
+
+    protected void setValue(WebElement element, String value) {
+        clickOn(element);
+        element.clear();
+        element.sendKeys(value);
+    }
+
+    protected void setValue(By locator, String value) {
+        clickOn(locator);
+        $(locator).clear();
+        $(locator).sendKeys(value);
+    }
+
+    protected void clickOn(WebElement element) {
+        waitFor(elementToBeClickable(element)).click();
+    }
+
+    protected void clickOn(By locator) {
+        $(locator, Conditions.CLICKABLE).click();
     }
 
     protected <T> T waitFor(ExpectedCondition<T> condition, long timeout) {
